0d62ca4f622df5eb615c68c3b262399d1a5639bd
Asmir Kopic <asmir.kopic123@gmail.com>
2022-03-19 18:49:48 +0100

    added increment, decrement and remove button in cart detail

--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -86,4 +86,31 @@ export class CartService {
 
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantyValue}`) 
   }
+
+  // method for decrement quantity in cart details
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+
+    if (theCartItem.quantity == 0) {
+      this.remove(theCartItem);
+    } else {
+      this.computeCartTotals();
+    }
+
+  }
+
+  remove(theCartItem: CartItem) {
+
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id );
+
+    // if found, remove the item from the array at the given index
+    if(itemIndex > -1){
+      this.cartItems.splice(itemIndex, 1); // remove item index
+
+      this.computeCartTotals(); 
+    }
+  }
+
+
 }
